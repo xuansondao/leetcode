de9f85ec8c1bc7dc9b6415f738f5632dc330020f
sondx <daoxuansonuet@gmail.com>
2022-05-19 00:46:15 +0700

    UPDATE CODE

--- src/main/java/com/daoxuanson/LeetCode1422.java
+++ src/main/java/com/daoxuanson/LeetCode1422.java
@@ -1,7 +1,27 @@
 package com.daoxuanson;
 
 public class LeetCode1422 {
-    public int maxScore(String s) {
+    public static int maxScore(String s) {
+        int numOfOne = 0;
+        int numOfZero = s.charAt(0) == '0' ? 1 : 0;
+        for(int i = 1; i<s.length(); i++){
+            if(s.charAt(i) == '1'){
+                numOfOne++;
+            }
+        }
+        int sum = numOfOne + numOfZero;
+        for(int i = 1; i<s.length(); i++){
+            sum = Math.max(sum, numOfOne+numOfZero);
+            if(s.charAt(i) == '1'){
+                numOfOne--;
+            }else{
+                numOfZero++;
+            }
+        }
+        return sum;
+    }
 
+    public static void main(String[] args) {
+        System.out.println(maxScore("011101"));
     }
 }

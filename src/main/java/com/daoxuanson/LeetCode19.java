59d06f3b4e682cfc46df03ab73f1febc86436099
sondx <daoxuansonuet@gmail.com>
2022-06-17 17:48:34 +0700

    update code LeetCode19

--- src/main/java/com/daoxuanson/LeetCode19.java
+++ src/main/java/com/daoxuanson/LeetCode19.java
@@ -18,13 +18,31 @@ public class LeetCode19 {
         }
     }
 
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode rs = new ListNode();
-        int length = 0;
-        while (head != null) {
-            length++;
-            head = head.next;
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode p1 = head;
+        ListNode start = head;
+        for (; n > 0; n--)
+            p1 = p1.next;
+        if (p1 == null) return head.next;
+        while (p1.next != null) {
+            p1 = p1.next;
+            start = start.next;
         }
-        return rs;
+        start.next = start.next.next;
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        removeNthFromEnd(node1, 2);
+
     }
 }
